d5bddf7bd9cce0dba93585749b15376f18c03906
DavidAML3 <david.aml.3@hotmail.com>
2023-01-23 09:19:57 -0500

    initial commit

---
+++ SwiftUI_Project/SwiftUI_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ProjectApp.swift
+//  SwiftUI_Project
+//
+//  Created by David Mejía on 23/01/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
